c8610441d286543bc81172d18b7ebe1d0d8d07ee
timo.buechert <timo.buechert@gmail.com>
2019-03-10 22:30:35 -0400

    worked on join room functionality, added data mockup on serverside to replace with db check

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { secure: false },
-    genid: function (request) { return uuid(); }
+    genid: function (request) {
+        return uuid();
+    }
 }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,10 +30,10 @@ app.get('/', function (req, res) {
 
 app.post('/login', function (request, response) {
     if (checkUserPassword(request.body.emailLogin, request.body.pswLogin)) {
-        if(firstLogin(request.body.emailLogin)){
+        if (firstLogin(request.body.emailLogin)) {
             request.session.username = request.body.emailLogin;
             response.redirect('/setup');
-        }else{
+        } else {
             request.session.username = request.body.emailLogin;
             response.redirect('/dashboard');
 
@@ -45,82 +47,119 @@ app.post('/login', function (request, response) {
 });
 
 app.get('/dashboard', function (req, res) {
-    if (req.session.username && roomAlreadyRegistered(req.session.username)){ //check if room registered!!
+    if (req.session.username && roomAlreadyRegistered(req.session.username)) { //check if room registered!!
         res.sendFile(path.join(__dirname + '/private/dashboard.html'));
-    }  else {
+    } else {
         res.redirect('/login.html');
     }
 });
 
 app.get('/setup', function (req, res) {
-    if (req.session.username){
+    if (req.session.username) {
         res.sendFile(path.join(__dirname + '/private/setUp.html'));
-    }  else {
+    } else {
         res.redirect('/login.html');
     }
 });
 
 
 app.post('/register', function (req, res) {
-    console.log( 
-    req.body.emailRegister + 
-    req.body.pswRegister + 
-    req.body.nameFirstRegister + 
-    req.body.nameLastRegister + 
-    req.body.adressRegister +
-    req.body.schoolRegister );
-    if (register(req.body)) {
-        res.sendStatus(200);
+    if (req.session.username) {
+        console.log(
+            req.body.emailRegister +
+            req.body.pswRegister +
+            req.body.nameFirstRegister +
+            req.body.nameLastRegister +
+            req.body.adressRegister +
+            req.body.schoolRegister);
+        if (register(req.body)) {
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(403);
+        }
     } else {
         res.sendStatus(403);
     }
-    
+
 })
 
 app.get('/calendar', function (req, res) {
     if (req.session.username) {
         res.send('Yahoo!');
         console.log('Authorized request');
-    }else{
+    } else {
         console.log('Unauthorizeds request');
     }
 });
 
 app.post('/checkRoomNameAvailable/:roomName', function (req, res) {
-    if (roomNameExists(req.params.roomName)) {
-        console.log(req.params.roomName);
-        res.send('false');
+    if (req.session.username) {
+
+        if (roomNameExists(req.params.roomName)) {
+            console.log(req.params.roomName);
+            res.send('false');
+        } else {
+            res.send('true');
+        }
     } else {
-        res.send('true');
+        res.sendStatus(403);
     }
 });
 
 app.post('/registerRoom/:roomName', function (req, res) {
     console.log('register Room');
-    if (registerRoom(req.params.roomName, req.session.username)) {
-        res.send('true');
+    if (req.session.username) {
+        if (registerRoom(req.params.roomName, req.session.username)) {
+            res.send('true');
+        } else {
+            res.send('false');
+        }
+
     } else {
-        res.send('false');
+        res.sendStatus(403);
     }
 });
+
+app.post('/getRooms/:search', function (req, res) {
+    if (req.session.username) {
+        res.json(getRoomsFromDB(req.params.search))
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+app.post('/joinRoom', function (req, res) {
+    console.log(req.params.roomName);
+    console.log(req.body.psw);
+    console.log(req.session.username);
+    if (req.session.username) {
+        if (joinRoom(req.body.roomName, req.body.psw, req.session.username)) {
+            res.send('true');
+        } else {
+            res.send('false');
+        }
+    } else {
+        res.sendStatus(403);
+    }
+  });
 
 app.listen(app.get('port'), function () {
     console.log('Server started up and is now listening on port:' + app.get('port'));
 });
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.status(404);
     res.render('landing_message', {
         message: 'Sorry, this page does not exist.'
     });
- });
+});
 
- app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(500);
     res.render('landing_message', {
         message: 'Sorry, an internal error occured.'
     });
- });
+});
 
 function roomAlreadyRegistered(email) {
     return true; //implement db check here later
@@ -146,7 +185,7 @@ function firstLogin(email) {
 
 function roomNameExists(roomName) {
     //implement db check here
-    if (roomName===':default') {
+    if (roomName === ':default') {
         return false;
     } else {
         return true;
@@ -155,11 +194,32 @@ function roomNameExists(roomName) {
 
 function registerRoom(roomName, userName) {
     //implement db check here
-    if (roomName===':default') {
-        console.log('Room registered: ' + roomName +' with User '+ userName);
+    if (roomName === ':default') {
+        console.log('Room registered: ' + roomName + ' with User ' + userName);
         return true;
     } else {
         console.log('something')
+        return false;
+    }
+}
+
+function getRoomsFromDB(search) {
+    //implement db lookup with search here
+    return {
+        "rooms": [{
+            "name": "test",
+            "adress": "simcoe"
+        }, {
+            "name": "test2",
+            "adress": "south"
+        }]
+    };
+}
+
+function joinRoom(roomName, psw, username) {
+    if (roomName == 'test' && psw == 'test' && username == 'timo.buechert@uoit.net') {
+        return true;
+    }else{
         return false;
     }
 }
--- public/scripts/setUp.js
+++ public/scripts/setUp.js
@@ -2,8 +2,9 @@ $(document).ready(function () {
     $('#createNew').hide();
     $('#joinExisting').hide();
     $('#lastStep').hide();
-    
-    $('#newRoom').click(function (e) { 
+    $('#joinRoomButton').hide();
+
+    $('#newRoom').click(function (e) {
         $('#stepOne').hide({
             duration: 'fast',
             easing: 'swing'
@@ -14,10 +15,10 @@ $(document).ready(function () {
         });
         $('#secondPoint').addClass('active');
         $('#firstPoint').removeClass('active');
-        
+
     });
 
-    $('#joinRoom').click(function (e) { 
+    $('#joinRoom').click(function (e) {
         $('#stepOne').hide({
             duration: 'fast',
             easing: 'swing'
@@ -26,50 +27,101 @@ $(document).ready(function () {
             duration: 'fast',
             easing: 'swing'
         });
-        
+
         $('#secondPoint').addClass('active');
         $('#firstPoint').removeClass('active');
     });
 
-    $('#createRoom').click(function (e) { 
-        $.post("/registerRoom/:"+ $('#roomName').val(), function (data) {
-                if (data==='true') {
-                    $('#createNew').hide({
-                        duration: 'fast',
-                        easing: 'swing'
-                    });
-                    $('#lastStep').show({
-                        duration: 'fast',
-                        easing: 'swing'
-                    });
-                    $('#finalMessage').html('You`re all set!');
-                    $('#thirdPoint').addClass('active');
-                    $('#secondPoint').removeClass('active');
-                    setTimeout(function () {
-                        window.location.replace('/dashboard');
-                    }, 1000)
-                } else {
-                }
+    $('#createRoom').click(function (e) {
+        $.post("/registerRoom/:" + $('#roomName').val(), function (data) {
+            if (data === 'true') {
+                $('#createNew').hide({
+                    duration: 'fast',
+                    easing: 'swing'
+                });
+                $('#lastStep').show({
+                    duration: 'fast',
+                    easing: 'swing'
+                });
+                $('#finalMessage').html('You`re all set!');
+                $('#thirdPoint').addClass('active');
+                $('#secondPoint').removeClass('active');
+                setTimeout(function () {
+                    window.location.replace('/dashboard');
+                }, 1000)
+            } else {
+            }
         });
-        
+
     });
 
-  
 
-    $('#roomName').keyup(function (e) { 
+
+    $('#roomName').keyup(function (e) {
         $.ajax({
             type: "post",
             url: "/checkRoomNameAvailable/:" + $('#roomName').val(),
             data: "data",
             dataType: "text",
             success: function (response) {
-                if (response==='true') {
+                if (response === 'true') {
                     $('#roomNameInfo').html('<br>');
                 } else {
                     $('#roomNameInfo').html('Roomname not available.');
                 }
             }
         });
-        
+
+    });
+
+
+
+    $('#roomNameJoin').keyup(function (e) {
+        $.ajax({
+            type: "post",
+            url: "/getRooms/:" + $('#roomNameJoin').val(),
+            data: "data",
+            dataType: "json",
+            success: function (response) {
+                var found = false;
+                for (let i = 0; i < response.rooms.length; i++) {
+                    if (response.rooms[i].name == $('#roomNameJoin').val()) {
+                        found = true;
+                    }
+                }
+                if (found) {
+                    $('#joinRoomInfo').html('Room found.');
+                    $('#joinRoomButton').show();
+                }else{
+                    $('#joinRoomInfo').html('');
+                    $('#joinRoomButton').hide();
+                }
+            }
+        });
+    });
+
+    $('#joinRoomButton').click(function () {
+
+        $.post("/joinRoom",{ psw: $('#roomPasswordJoin').val(), roomName: $('#roomNameJoin').val() }, function (data) {
+            if (data === 'true') {
+                $('#joinExisting').hide({
+                    duration: 'fast',
+                    easing: 'swing'
+                });
+                $('#lastStep').show({
+                    duration: 'fast',
+                    easing: 'swing'
+                });
+                $('#finalMessage').html('You`re all set!');
+                $('#thirdPoint').addClass('active');
+                $('#secondPoint').removeClass('active');
+                setTimeout(function () {
+                    window.location.replace('/dashboard');
+                }, 1000);
+            } else {
+                $('#roomPasswordJoinInfo').html('Incorrect password.');
+            }
+        });
+
     });
 });
