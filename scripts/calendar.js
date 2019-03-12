cb73e6e54b26bb8786d69536e2bf3fab38dd8b4c
Sumeet Dhillon <32371636+sumeet98@users.noreply.github.com>
2019-03-12 13:33:42 -0400

    changed calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,79 +1,153 @@
-function displayCalendar(){
+window.onload = function() {
+  //Copyright 1996 - Tomer and Yehuda Shiran
+  //Feel free to "steal" this code provided that you leave this notice as is.
+
+  //^^^^ I CHANGED MOST OF IT UP USED THEIR TEMPLATE ONLY
 
 
- var htmlContent ="";
- var FebNumberOfDays ="";
- var counter = 1;
+  setCal()
 
 
- var currentDate = new Date();
- var month = currentDate.getMonth();
+  function leapYear(year) {
+  if (year % 4 == 0)
+  return true
+  return false
+  }
 
- var nextMonth = month+1;
- var prevMonth = month -1;
- var day = currentDate.getDate();
- var year = currentDate.getFullYear();
+  function getDays(month, year) {
+  var ar = new Array(12)
+  ar[0] = 31
+  ar[1] = (leapYear(year)) ? 29 : 28
+  ar[2] = 31
+  ar[3] = 30
+  ar[4] = 31
+  ar[5] = 30
+  ar[6] = 31
+  ar[7] = 31
+  ar[8] = 30
+  ar[9] = 31
+  ar[10] = 30
+  ar[11] = 31
+
+  return ar[month]
+  }
+
+  function getMonthName(month) {
+  var ar = new Array(12)
+  ar[0] = "January"
+  ar[1] = "February"
+  ar[2] = "March"
+  ar[3] = "April"
+  ar[4] = "May"
+  ar[5] = "June"
+  ar[6] = "July"
+  ar[7] = "August"
+  ar[8] = "September"
+  ar[9] = "October"
+  ar[10] = "November"
+  ar[11] = "December"
+
+  return ar[month]
+  }
+
+  function setCal() {
+  var now = new Date()
+  var year = now.getYear()
+  if (year < 1000)
+  year+=1900
+  var month = now.getMonth()
+  var monthName = getMonthName(month)
+  var date = now.getDate()
+  now = null
+
+  var firstDayInstance = new Date(year, month, 1)
+  var firstDay = firstDayInstance.getDay()
+  firstDayInstance = null
+
+  var days = getDays(month, year)
+
+  drawCal(firstDay + 1, days, date, monthName, year)
+  }
+
+  function drawCal(firstDay, lastDate, date, monthName, year) {
+  // constant table settings
+  var headerHeight = 50 // height of the table's header cell
+  var border = 2 // 3D height of table's border
+  var cellspacing = 4 // width of table's border
+  var headerColor = "black" // color of table's header
+  var headerSize = "+3" // size of tables header font
+  var colWidth = 60 // width of columns in table
+  var dayCellHeight = 25 // height of cells containing days of the week
+  var dayColor = "black" // color of font representing week days
+  var cellHeight = 40 // height of cells representing dates in the calendar
+  var todayColor = "red" // color specifying today's date in the calendar
 
 
+  // create basic table structure
+  var text = "" // initialize accumulative variable to empty string
+  text += '<CENTER>'
+  text += '<TABLE BORDER=' + border + ' CELLSPACING=' + cellspacing + '>' // table settings
+  text += '<TH COLSPAN=7 HEIGHT=' + headerHeight + '>' // create table header cell
+  text += '<FONT COLOR="' + headerColor + '" SIZE=' + headerSize + '>' // set font for table header
+  text += monthName + ' ' + year
+  text += '</FONT>' // close table header's font settings
+  text += '</TH>' // close header cell
 
- if (month == 1){
-    if ( (year%100!=0) && (year%4==0) || (year%400==0)){
-      FebNumberOfDays = 29;
-    }else{
-      FebNumberOfDays = 28;
-    }
- }
+  // variables to hold constant settings
+  var openCol = '<TD WIDTH=' + colWidth + ' HEIGHT=' + dayCellHeight + '>'
+  openCol += '<FONT COLOR="' + dayColor + '">'
+  var closeCol = '</FONT></TD>'
 
+  // create array of abbreviated day names
+  var weekDay = new Array(7)
+  weekDay[0] = "Sun"
+  weekDay[1] = "Mon"
+  weekDay[2] = "Tues"
+  weekDay[3] = "Wed"
+  weekDay[4] = "Thu"
+  weekDay[5] = "Fri"
+  weekDay[6] = "Sat"
 
+  // create first row of table to set column width and specify week day
+  text += '<TR ALIGN="center" VALIGN="center">'
+  for (var dayNum = 0; dayNum < 7; ++dayNum) {
+  text += openCol + weekDay[dayNum] + closeCol
+  }
+  text += '</TR>'
 
- var monthNames = ["January","February","March","April","May","June","July","August","September","October","November", "December"];
- var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday", "Saturday"];
- var dayPerMonth = ["31", ""+FebNumberOfDays+"","31","30","31","30","31","31","30","31","30","31"]
+  // declaration and initialization of two variables to help with tables
+  var digit = 1
+  var curCell = 1
 
+  for (var row = 1; row <= Math.ceil((lastDate + firstDay - 1) / 7); ++row) {
+  text += '<TR ALIGN="right" VALIGN="top">'
+  for (var col = 1; col <= 7; ++col) {
+  if (digit > lastDate)
+  break
+  if (curCell < firstDay) {
+  text += '<TD></TD>';
+  curCell++
+  } else {
+  if (digit == date) { // current cell represent today's date
+  text += '<TD HEIGHT=' + cellHeight + '>'
+  text += '<FONT COLOR="' + todayColor + '">'
+  text += digit
+  text += '</FONT><BR>'
+  text += '</FONT>'
+  text += '</TD>'
+  } else
+  text += '<TD HEIGHT=' + cellHeight + '>' + digit + '</TD>'
+  digit++
+  }
+  }
+  text += '</TR>'
+  }
 
+  // close all basic table tags
+  text += '</TABLE>'
+  text += '</CENTER>'
 
- var nextDate = new Date(nextMonth +' 1 ,'+year);
- var weekdays= nextDate.getDay();
- var temp = weekdays
- var numOfDays = dayPerMonth[month];
-
-
-
-
- while (weekdays>0){
-    htmlContent += "<td class='monthPre'></td>";
-     weekdays--;
- }
-
-
- while (counter <= numOfDays){
-    if (temp > 6){
-        temp = 0;
-        htmlContent += "</tr><tr>";
-    }
-
-    if (counter == day){
-        htmlContent +="<td class='dayNow'  onMouseOver='this.style.background=\"blue\"; this.style.color=\"white\"' "+
-        "onMouseOut='this.style.background=\"white\"; this.style.color=\"black\"'>"+counter+"</td>";
-    }else{
-        htmlContent +="<td class='monthNow' onMouseOver='this.style.background=\"blue\"'"+
-        " onMouseOut='this.style.background=\"white\"'>"+counter+"</td>";
-
-    }
-
-    temp++;
-    counter++;
- }
-
-
- var calendarBody = "<table class='calendar'> <tr class='monthNow'><th colspan='7'>"
- +monthNames[month]+" "+ year +"</th></tr>";
- calendarBody +="<tr class='dayNames'>  <td>Sun</td>  <td>Mon</td> <td>Tues</td>"+
- "<td>Wed</td> <td>Thurs</td> <td>Fri</td> <td>Sat</td> </tr>";
- calendarBody += "<tr>";
- calendarBody += htmlContent;
- calendarBody += "</tr></table>";
-
- document.getElementById("calendar").innerHTML=calendarBody;
-
+  // print accumulative HTML string
+  document.write(text)
+}
 }
